dad892bff3c59a9d6f380f99043c418394ff621d
muhammadreza abdollahnejad <reza123.reza987@gmail.com>
2023-03-15 12:50:16 +0330

    name change.

--- src/maktab_questiion_one/DemoTurners2.java
+++ src/maktab_questiion_one/DemoTurners2.java
@@ -2,8 +2,10 @@ package maktab_questiion_one;
 
 import static maktab_questiion_one.Leaf.Color.GREEN;
 import static maktab_questiion_one.Pancake.Face.head;
+import static maktab_questiion_one.Season.season.winter;
+import static maktab_questiion_one.TimeOfTheDay.times.morning;
 
-public class DemoTurners {
+public class DemoTurners2 {
     public static void main(String[] args) {
         Leaf leaf=new Leaf(GREEN);
         leaf.turn();
@@ -11,5 +13,9 @@ public class DemoTurners {
         page.turn();
         Pancake pancake=new Pancake(head);
         pancake.turn();
+        TimeOfTheDay timeOfTheDay=new TimeOfTheDay(morning);
+        timeOfTheDay.turn();
+        Season season=new Season(winter);
+        season.turn();
     }
 }
